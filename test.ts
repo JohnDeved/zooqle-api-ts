45ebbc04249d3f860067f3d24329513dd0df4cae
Undefined <johann@objekt.stream>
2018-10-08 18:41:16 +0200

    + update tests

--- test.ts
+++ test.ts
@@ -1,14 +1,8 @@
 import { zooqle } from '.'
 
-zooqle.search('115E8B3596DE77BF6A463B2654697F47F4064DB6').then(console.log)
-
-// zooqle.search('tt0468569').then((response) => {
-//   console.log(response)
-//   debugger
-// })
-
-// zooqle.search('tt2575988').then(console.log)
-
-// zooqle.getHrefData('/misc/tveps.php?show=60573&se=5&ep=1').then(console.log)
-
-// zooqle.getTorrentData('/batman-the-dark-knight-2008-2160p-x265-10bit-s90-joy-wqipm.html').then(console.log)
+zooqle.search('115E8B3596DE77BF6A463B2654697F47F4064DB6').then(response => console.log(1, response.type))
+zooqle.search('tt2575988').then(response => console.log(2, response.type))
+zooqle.search('silicon valley').then(response => console.log(3, response.type))
+zooqle.search('tt0468569').then(response => console.log(4, response.type))
+zooqle.getHrefData('/misc/tveps.php?show=60573&se=5&ep=1').then(response => console.log(5, response[0].hash))
+zooqle.getTorrentData('/batman-the-dark-knight-2008-2160p-x265-10bit-s90-joy-wqipm.html').then(response => console.log(6, response.hash))
