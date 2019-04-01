a5378dc905e35eb3fdbe6b98403287ebd90c792b
Undefined <johann@objekt.stream>
2019-04-01 20:28:23 +0200

    + debug node script

---
+++ debug.ts
@@ -0,0 +1,4 @@
+import { zooqle } from './index'
+
+zooqle.search('tt0468569').then(console.log).then(() => { debugger }).catch(console.error)
+zooqle.search('tt0076759').then(console.log).then(() => { debugger }).catch(console.error)
